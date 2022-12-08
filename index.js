7f7a0ea9ab4a72e40a1361c675c5b23daeadd840
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-08 09:52:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,15 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+   //arr = tutorials.map(str =>  str.charAt(0).toUpperCase() + str + str.slice(1));
+  let tutor
+  let caps = tutorials.map(string => {
+    tutor = string.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1))
+   return tutor.join(' ')})
+   //debugger
+    return caps
+
 }
+
+}
+console.log(titleCased())
